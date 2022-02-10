711bf77b060c5bb857280f27713421db3eb77c71
kotoldadas <bekmez16@itu.edu.tr>
2022-02-10 06:56:20 +0300

    background transparency disabled and some formatting stuf

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -13,5 +13,5 @@ vim.g.vscode_style = "dark"
 -- For light theme
 -- vim.g.vscode_style = "light"
 -- -- Enable transparent background.
-vim.g.vscode_transparent = 1
+-- vim.g.vscode_transparent = 1
 vim.cmd([[colorscheme vscode]])
